e2adb5e259a63b647b332d8db4b8032dba7013f5
ryota-1231 <rrrrr.1231@gmail.com>
2022-01-26 11:41:00 +0900

    first commit

---
+++ pages/api/hello.tsx
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+type Data = {
+  name: string
+}
+export default function Hello (req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: 'Hello' })
+}
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,10 +3,11 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticPaths, GetStaticProps } from 'next'
 
 export default function Post({ postData }) {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>{postData.title}</title>
       </Head>
@@ -21,7 +22,7 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -29,7 +30,7 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id)
   return {
     props: {
